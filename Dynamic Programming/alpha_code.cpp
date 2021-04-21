523cf67f429049270bf90617a8a2ea7b572f43bb
vitarka101 <kumar.ayushds@gmail.com>
2021-04-21 19:47:02 +0530

    Using Dynamic Programming

--- Dynamic Programming/alpha_code.cpp
+++ Dynamic Programming/alpha_code.cpp
@@ -16,6 +16,25 @@ int num_code(int *arr , int n)
 	return count;
 }
 
+int num_code_DP(int *arr , int n ,int *output)
+{
+        // base case
+	// Here n is the remaining number from the start(since we are decreasing the size of array from the end) of the array whose num_code is to be calculated 
+
+        if( n == 0 || n == 1)
+                return 1;
+
+	if( output[n] > 0)
+		return output[n];
+
+        int count  = num_code(arr , n-1);
+
+        if(arr[n-2]*10 + arr[n-1] <= 26)
+                count +=  num_code(arr , n-2 );
+
+	output[n] = count;
+        return count;
+}
 
 int main()
 {
@@ -32,5 +51,8 @@ int main()
 	}
 
 	cout<< num_code(arr,s.size())<<endl;
+	
+	int *output = new int[s.size()+1];  // size 0 to n
+	cout<< num_code_DP(arr,s.size(),output)<<endl;
 	return 0;
 }
