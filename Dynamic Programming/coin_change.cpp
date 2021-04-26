f2c3f9b468bfae14fcdf5b108b19c9bbb6c8a43a
vitarka101 <kumar.ayushds@gmail.com>
2021-04-26 19:25:22 +0530

    Using Dynamic Programming

--- Dynamic Programming/coin_change.cpp
+++ Dynamic Programming/coin_change.cpp
@@ -16,6 +16,31 @@ int coin_change(int n,int *d,int numD)
 	return first+second;
 }
 
+
+int coin_changeDP(int n,int *d,int numD,int **arr)
+{
+        if(n<0)
+                return 0;
+        if(n==0)
+                return 1;
+        if(numD==0 || numD<0)
+                return 0;
+	if(arr[n][numD] > -1) // 0 is a possibility
+		return arr[n][numD];
+
+        int first = coin_change(n-d[0],d,numD);
+        int second = coin_change(n,d+1,numD-1);
+
+	int output = first + second;
+	
+	arr[n][numD] = output;
+
+        return output;
+}
+
+
+
+
 int main()
 {
 	cout<<"Enter the change value : ";
@@ -26,14 +51,26 @@ int main()
 	int numD;
 	cin>>numD;
 
-	int *arr= new int[numD];
+	int *arr= new int[numD]();
 
 	cout<<"Enter the coin values : ";
 	for(int i=0;i<numD;i++)
 		cin>>arr[i];
+	
+	int **dp = new int*[n+1];
+        for(int i=0;i<n+1;i++)
+                dp[i] = new int[numD+1];
+
+	for(int i=0;i<n+1;i++)
+	{
+		for(int j=0;j<numD+1;j++)
+			dp[i][j] = -1;
+	}
+
+        cout<<"No of ways for coin change using DP : "<<coin_changeDP(n,arr,numD,dp)<<endl;
 
 	cout<<"No of ways for coin change : "<<coin_change(n,arr,numD)<<endl;
-
+	
 	return 0;
 }
 
