9945057d422e6ea2d2e6c3b16e035fc02eb43313
vitarka101 <kumar.ayushds@gmail.com>
2021-05-04 23:52:02 +0530

    Using Dynamic Programming

--- Dynamic Programming/magic_grid.cpp
+++ Dynamic Programming/magic_grid.cpp
@@ -3,68 +3,64 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int getStrength(int **grid,int n,int m,int si,int sj)
+int getStrength(int **grid,int n,int m,int si,int sj,int **health)
 {
    	int cost1 = INT_MAX;
 	int cost2 = INT_MAX;
+    
     if(si == n-1 && sj == m-1)
     {
         return 1;
     }
-
-    // Causes ERROR
-
-    /*if(si == n-1 && sj == m-2)
-    {
-        if(grid[si][sj] <= 0)
-        return 1 - grid[si][sj];
-        else
-            return 1;
-    }
-       
-    if(si == n-2 && sj == m-1)
-    {
-        if(grid[si][sj] <= 0)
-        return 1 - grid[si][sj];
-        else
-            return 1;
-    }*/
+    
+    if(health[si][sj] > 0)
+        return health[si][sj];
 
     if(sj+1 < m)
     {
-        cost1 = getStrength(grid,n,m,si,sj+1) - grid[si][sj+1];
-        if(cost1 == 0)
+        cost1 = getStrength(grid,n,m,si,sj+1,health) - grid[si][sj];
+        if(cost1 <= 0)
         {
             cost1 = 1;
-        }
-        if(cost1 < 0)
-        {
-            cost1 = -1*cost1;
         }
     }
      
     if(si+1 < n)
     {
-        cost2 = getStrength(grid,n,m,si+1,sj) - grid[si+1][sj];
-        if(cost2 == 0)
+        cost2 = getStrength(grid,n,m,si+1,sj,health) - grid[si][sj];
+        if(cost2 <= 0)
         {
             cost2 = 1;
-        }
-        if(cost2 < 0)
-        {
-            cost2 = -1*cost2;
         }
     }
      
 
-    return min(cost1,cost2);
+    health[si][sj] =  min(cost1,cost2);
+    
+    return health[si][sj];
 
 
 }
 
 int getMinimumStrength(int** grid, int n, int m) {
+	// Write your code here
+    if(n==0 || m==0)
+        return 1;
+    if(n==1 && m==1)
+    {
+        if(grid[0][0]>0)
+            return 1;
+        else
+            return 1-grid[0][0];
+    }
+    
+    int **health = new int *[n];
 
-    int ans = getStrength(grid,n,m,0,0);
+        for (int i = 0; i < n; ++i) {
+            health[i] = new int[m];
+        }
+
+    int ans = getStrength(grid,n,m,0,0,health);
     return ans;
 }
 
